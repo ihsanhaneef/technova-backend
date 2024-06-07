ea73d413755f53b80ab450a3b5bcff63860de1a7
ihsan.haneef <ihsanhaneef@gmail.com>
2024-06-08 00:14:52 +0530

    table data

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,15 @@
 //router admin
 import express from "express";
 import { authMiddleWare, login, profile, register } from "../controllers/user.js";
+import { usersDetails } from "../controllers/admin.js";
 
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/profile",authMiddleWare, profile)
+router.get("/profile",authMiddleWare, profile);
+router.get("AllUser", authMiddleWare, usersDetails);
 
 export const userRoute = router;
 
